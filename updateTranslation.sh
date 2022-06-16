cdd254225f628c3e0f98e62e21f95d5845dc2c2c
Eban Rami <account+gitil@eban.eu.org>
2022-06-16 13:25:55 +0000

    Update updateTranslation.sh, .gitlab-ci.yml

--- updateTranslation.sh
+++ updateTranslation.sh
@@ -1,18 +1,11 @@
-lastCommit=$(git rev-parse origin/master)
-lastCommitAuthor=$(git log -1 | grep Author)
+for i in $(ls -1 -p fr/ | grep -v / | sed -e 's/\..*$//')
+do
+  gettext-md -o i18n/__name__.pot --pot fr/$i.md
+  if test -f i18n/en/$i.po; then
+    msgmerge --backup=none -U i18n/en/$i.po i18n/$i.pot
+  else
+    msginit --no-translator -i i18n/$i.pot -o i18n/en/$i.po
+    sed -i 's/Language: fr/Language: en/' i18n/en/$i.po
+  fi
+done
 
-echo ${lastCommitAuthor}
-echo ${lastCommitAuthor} | grep -q "account+gitil@eban.eu.org" | echo $?
-
-if echo ${lastCommitAuthor} | grep -q "contact@ilearned.eu"; then
-  for i in $(ls -1 -p fr/ | grep -v / | sed -e 's/\..*$//')
-  do
-    gettext-md -o i18n/__name__.pot --pot fr/$i.md
-    if test -f i18n/en/$i.po; then
-      msgmerge --backup=none -U i18n/en/$i.po i18n/$i.pot
-    else
-      msginit --no-translator -i i18n/$i.pot -o i18n/en/$i.po
-      sed -i 's/Language: fr/Language: en/' i18n/en/$i.po
-    fi
-  done
-fi
