736a64ef6017029e93a9fba8e6e53c5a155a25eb
Eban Rami <contact+git@eban.eu.org>
2022-06-16 12:37:36 +0200

    [🔁 CI] Add i18n CI !

---
+++ updateTranslation.sh
@@ -0,0 +1,10 @@
+for i in $(ls -1 -p fr/ | grep -v / | sed -e 's/\..*$//')
+do
+  gettext-md -o i18n/__name__.pot --pot fr/$i.md
+  if test -f i18n/en/$i.po; then
+    msgmerge --backup=none -U i18n/en/$i.po i18n/$i.pot
+  else
+    msginit --no-translator -i i18n/$i.pot -o i18n/en/$i.po
+    sed -i 's/Language: fr/Language: en/' i18n/en/$i.po
+  fi
+done
