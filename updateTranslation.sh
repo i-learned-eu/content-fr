cf21c10c9fbd718dcfa9714dadc2327385b21b7d
Eban Rami <account+gitil@eban.eu.org>
2022-06-16 13:37:11 +0000

    Update .gitlab-ci.yml, updateTranslation.sh

--- updateTranslation.sh
+++ updateTranslation.sh
@@ -1,6 +1,6 @@
 for i in $(ls -1 -p fr/ | grep -v / | sed -e 's/\..*$//')
 do
-  $(npm bin)/gettext-md -o i18n/__name__.pot --pot fr/$i.md
+  gettext-md -o i18n/__name__.pot --pot fr/$i.md
   if test -f i18n/en/$i.po; then
     msgmerge --backup=none -U i18n/en/$i.po i18n/$i.pot
   else
